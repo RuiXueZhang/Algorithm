1dc3463d53fee9cd6a9b92b46f2d64c1a034c42f
RuiXue Zhang <zhangruixue@haima.me>
2020-06-04 14:45:05 +0800

    first commit

---
+++ Pre/src/test/java/pre/PreApplicationTests.java
@@ -0,0 +1,13 @@
+package pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
